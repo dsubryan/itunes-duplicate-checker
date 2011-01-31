501e715b7f7cad9143a7c5f1a859865055add5af
dsubryan@gmail.com <dsubryan@gmail.com@f6488a08-83e3-4fd4-c244-b5c20db7ca91>
2011-01-31 18:44:23 +0000

    Added Console project to provide a quick test for iTunes functions.  I could not add a test project because it VS 2010 would not allow me to switch the target framework to 3.5.

---
+++ iTunes/iTunes.Duplicate.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTunes.Duplicate.Gui;
+using iTunesLib;
+
+namespace iTunes.Duplicate.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchString = "hold's it against me";
+            Gui.iTunes iTunesApp = new Gui.iTunes();
+            System.Console.WriteLine(iTunesApp.Search(searchString, ITPlaylistSearchField.ITPlaylistSearchFieldVisible).ToString());
+        }
+    }
+}
