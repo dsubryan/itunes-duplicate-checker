6f6907507be6d5c4b1f09ebee9000eca4a91f3c3
dsubryan <dsubryan@gmail.com>
2015-03-17 15:56:41 -0400

    Add try/catch to console application.

--- iTunes/iTunes.Duplicate.Console/Program.cs
+++ iTunes/iTunes.Duplicate.Console/Program.cs
@@ -11,11 +11,20 @@ namespace iTunes.Duplicate.Console
     {
         static void Main(string[] args)
         {
-            string searchString = "dog days flo";
-            string sourceDirectory = @"C:\Users\subryad\Downloads\VA-Hed_Kandi_Presents_Athmes_and_Artwork-(HEDK001BOX)-4CD-Retail-2011-HFT";
-            string destinationDirectory = @"C:\";
-            Gui.iTunes iTunesApp = new Gui.iTunes(sourceDirectory, destinationDirectory, Properties.Settings.Default.TitleFilters);
-            iTunesApp.CheckLibraryForDuplicates(searchString);
+            try
+            {
+                string searchString = "dog days flo";
+                string sourceDirectory = @"C:\Users\subryad\Downloads\VA-Hed_Kandi_Presents_Athmes_and_Artwork-(HEDK001BOX)-4CD-Retail-2011-HFT";
+                string destinationDirectory = @"C:\";
+                Gui.iTunes iTunesApp = new Gui.iTunes(sourceDirectory, destinationDirectory, Properties.Settings.Default.TitleFilters);
+                iTunesApp.CheckLibraryForDuplicates(searchString);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+
+            }
+
 
         }
     }
